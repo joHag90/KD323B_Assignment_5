a90bc78795e739ea96acf13974516a7bf8725a92
JoelHenning <joelhenninghagenblad@hotmail.com>
2015-04-30 11:02:27 +0200

    Klar och redovisad

---
+++ app/src/main/java/se/k3/antonochisak/kd323bassignment5/adapters/MyAdapter.java
@@ -0,0 +1,100 @@
+package se.k3.antonochisak.kd323bassignment5.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import se.k3.antonochisak.kd323bassignment5.R;
+import se.k3.antonochisak.kd323bassignment5.helpers.StaticHelpers;
+import se.k3.antonochisak.kd323bassignment5.models.movie.Movie;
+
+/**
+ * Created by JoelHenning on 15-04-27.
+ */
+public class MyAdapter extends BaseAdapter {
+
+    ArrayList<Movie> mMovies;
+    LayoutInflater mLayoutInflater;
+    private int mItemWidth, mItemHeight, mMargin;
+
+    public MyAdapter(ArrayList<Movie> mMovies, LayoutInflater mLayoutInflater) {
+        this.mMovies = mMovies;
+        this.mLayoutInflater = mLayoutInflater;
+    }
+
+    // We always use a viewholder pattern on listviews!
+    class ViewHolder {
+        @InjectView(R.id.poster)
+        ImageView poster;
+
+        @InjectView(R.id.textView_title)
+        TextView title;
+
+        @InjectView(R.id.textView_year)
+        TextView year;
+
+        @InjectView(R.id.textView_slugline)
+        TextView slugline;
+
+        public ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+
+            int screenWidth = StaticHelpers.getScreenWidth(view.getContext());
+            mItemWidth = (screenWidth / 2);
+            mItemHeight = (int) ((double) mItemWidth / 0.677);
+            mMargin = StaticHelpers.getPixelsFromDp(view.getContext(), 2);
+        }
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+        if (view == null) {
+            view = mLayoutInflater.inflate(R.layout.trending_list_item, viewGroup, false);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+
+        holder.title.setText(mMovies.get(i).getTitle());
+
+        String year = String.valueOf(mMovies.get(i).getYear());
+        holder.year.setText(year);
+
+        holder.slugline.setText(mMovies.get(i).getSlugline());
+
+        // Load pictures with picasso
+        Picasso.with(view.getContext())
+                .load(mMovies.get(i).getPoster())
+                .resize(mItemWidth, mItemHeight)
+                .into(holder.poster);
+        return view;
+    }
+
+    @Override
+    public int getCount() {
+        return mMovies.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return mMovies.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+}
+
+
